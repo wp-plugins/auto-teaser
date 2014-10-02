a8e61136bedb18b5589c8793d49cfc09c17690a3
thaikolja <thaikolja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-02 15:39:01 +0000

    Release 0.1.
    git-svn-id: https://plugins.svn.wordpress.org/auto-teaser/trunk@1000713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ auto-teaser.php
@@ -0,0 +1,27 @@
+<?php
+	/**
+	 * Plugin Name:  Auto Teaser
+	 * Plugin URI:   http://www.koljanolte.com/wordpress/plugins/auto-teaser/
+	 * Description:  Automatically generates a teaser for posts, pages and custom post types.
+	 * Version:      0.1
+	 * Author:       Kolja Nolte
+	 * Author URI:   http://www.koljanolte.com
+	 * License:      GPLv2 or later
+	 * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
+	 */
+
+	/**
+	 * Stop script when the file is called directly.
+	 */
+	if(!function_exists("add_action")) {
+		return false;
+	}
+
+	/**
+	 * Include all PHP files in the includes directory.
+	 */
+	$include_files = glob(dirname(__FILE__) . "/includes/*.php");
+	foreach($include_files as $include_file) {
+		/** @noinspection PhpIncludeInspection */
+		include($include_file);
+	}
